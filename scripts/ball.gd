a99640ace4aa5fd00d55cf364f03c736f84f0ddd
MathewSchaeffer <103226089+MathewSchaeffer@users.noreply.github.com>
2025-07-05 22:03:32 +1000

    version 1 complete

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -7,6 +7,8 @@ var startingMovement: Vector2 = Vector2.ZERO
 var startArray: Array = [Vector2(-1,-1), Vector2(-1,1), Vector2(1,1), Vector2(1,-1)]
 var viewport: Vector2
 @onready var paddle: CharacterBody2D = $"../../Paddle"
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 
 
 func _ready() -> void:
@@ -33,9 +35,15 @@ func ball_collision_handler(delta: float) -> void:
 	var collision_info: KinematicCollision2D = move_and_collide(velocity * delta)
 	if collision_info: # We only want to bounce if the ball actually collided.
 		SignalBus.shake.emit()
+		audio_stream_player.play()
+		#var pitch: float = 0
+		#pitch = audio_stream_player.pitch_scale * randf_range(0.8, 1.2)
+		#audio_stream_player.pitch_scale = pitch
+		audio_stream_player.pitch_scale *= 1.005
 		velocity = velocity.bounce(collision_info.get_normal())
 		var collider: Brick = collision_info.get_collider() as Brick
 		if collider is Brick:
+			
 			velocity *= 1.005
 			collider.hit(1)
 			if !collider.hp == 0:
